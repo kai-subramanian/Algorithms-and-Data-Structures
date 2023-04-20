a938976be72a1af307b2a901745df90c7ed1ac40
kai-subramanian <skailash27082k@gmail.com>
2023-04-20 10:14:17 +0530

    added check for boundary cases

--- Python/Searching Algorithms/ceil.py
+++ Python/Searching Algorithms/ceil.py
@@ -1,24 +1,24 @@
 # find the ceiling of a number, basically, the 
 # first number greater than or equal to the target.
 
-
-def ceil_find(arr,t,s,f):
-    # condition - greater than previous num, 
-    # lesser than next num
+def ceiling(arr,t):
     if (t>arr[len(arr)-1]):
         return -1
     elif (t<arr[0]):
         return arr[0]
-    m = (s+f)//2
+    s=0
+    f=len(arr)-1
+    ans=-1
     while(s<=f):
-        if (arr[m]<=t and arr[m+1]>t):
-            return arr[m+1]
-        elif (arr[m]>=t):
-            m-=1
+        m=s+(f-s)//2
+        if(arr[m]>t):
+            f=m-1
+        elif(arr[m]<t):
+            ans=m
+            s=m+1
         else:
-            m+=1
-    return arr[m]
-
+            return m
+    return ans+1
 l = [1, 2, 3, 5, 7, 8, 9, 10, 22, 23]
-target = 9
-print(ceil_find(l,target,0,len(l)))
+target = 999
+print(ceiling(l,target))
