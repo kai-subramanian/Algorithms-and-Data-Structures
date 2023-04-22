5a6c9aeb0f5b84d12c06f72f1f84ddd21ad4f6b4
kai-subramanian <skailash27082k@gmail.com>
2023-04-22 11:18:49 +0530

    search for smallest letter greater than target

---
+++ C++/Searching/smallestLetter.cpp
@@ -0,0 +1,28 @@
+// Program to print smallest letter in the array
+// which is greater than the target letter
+// Basically a character implementation of ceiling of array
+
+#include<iostream>
+using namespace std;
+
+char letterSearch(char arr[],char t,int s, int f){
+    int len=f;
+    while(s<=f){
+        int m = s+(f-s)/2;
+        if (arr[m]<t){
+            s=m+1;
+        }
+        else {
+            f=m-1;
+        }
+    }
+    return arr[s%len];
+}
+
+int main(){
+    char arr[]={'a','d','e','j','w','y'};
+    char target='g';
+    char res = letterSearch(arr,target,0,sizeof(arr)/sizeof(arr[0]));
+    cout<<res<<endl;
+    return 0;
+}
