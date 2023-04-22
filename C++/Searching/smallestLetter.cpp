ae9ee6790122e7527ee0c6abfb3e724541facf85
kai-subramanian <skailash27082k@gmail.com>
2023-04-22 11:20:29 +0530

    added equal to check, just for fun

--- C++/Searching/smallestLetter.cpp
+++ C++/Searching/smallestLetter.cpp
@@ -1,5 +1,5 @@
 // Program to print smallest letter in the array
-// which is greater than the target letter
+// which is greater than or equal to the target letter
 // Basically a character implementation of ceiling of array
 
 #include<iostream>
@@ -9,6 +9,9 @@ char letterSearch(char arr[],char t,int s, int f){
     int len=f;
     while(s<=f){
         int m = s+(f-s)/2;
+        if (arr[m]==t) {
+            return arr[m];
+        }
         if (arr[m]<t){
             s=m+1;
         }
@@ -21,7 +24,7 @@ char letterSearch(char arr[],char t,int s, int f){
 
 int main(){
     char arr[]={'a','d','e','j','w','y'};
-    char target='g';
+    char target='e';
     char res = letterSearch(arr,target,0,sizeof(arr)/sizeof(arr[0]));
     cout<<res<<endl;
     return 0;
