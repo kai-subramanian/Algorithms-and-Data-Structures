9b9a24406a908acec52e69143ed3fe6ed7d3df70
kai-subramanian <skailash27082k@gmail.com>
2023-04-23 15:41:29 +0530

    find peak element in mountain array

--- C++/Searching/peakElement.cpp
+++ C++/Searching/peakElement.cpp
@@ -5,17 +5,17 @@ using namespace std;
 int peak(int a[],int s,int f){
     while(s<f){
         int m=s+(f-s)/2;
-        if (a[m]>a[m+1]){
-            f=m;
-        }else{
+        if (a[m]<a[m+1]){
             s=m+1;
+        }else{
+            f=m;
         }
     }
     return f;
 }
 
 int main(){
-    int arr[]={8,21,24,30,15,6};
+    int arr[]={8,21,24,26,35,30,15,6};
     cout<<peak(arr,0,sizeof(arr)/sizeof(arr[0]))<<endl;
     return 0;
 }
