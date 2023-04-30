f833f7fb0640c8163258ccf6f99f8b99777c4b05
kai-subramanian <skailash27082k@gmail.com>
2023-04-30 16:09:59 +0530

    Added queue implementation

--- C++/Data Structures/Queue/queue.cpp
+++ C++/Data Structures/Queue/queue.cpp
@@ -1,70 +1,63 @@
-// Program to implement queue
+// Program to implement a queue from scratch
 #include<iostream>
 using namespace std;
 
 class Queue{
-    int *arr;
-    int size;
-    int front;
-    int tail;
-    int count;
  public:
-    Queue(int s);
-    void enqueue(int x);
-    int dequeue();
-    int peek();
+    int arr[10];
+    int item;
+    int front;
+    int rear;
+    Queue(){
+        front=0;
+        rear=0;
+    }
+    void enqueue(int x){
+        if (rear==10){
+            cout<<"OVERFLOW"<<endl;
+        }
+        else{
+            arr[rear++]=x;
+        }
+    }
+    void dequeue(){
+        if (front==rear){
+            cout<<"UNDERFLOW"<<endl;
+        }
+        else{
+            front++;
+        }
+    }
+    void peek(){
+        if (front>=0){
+            cout<<arr[front]<<endl;
+        }
+    }
 };
 
-Queue::Queue(int s){
-    size=s;
-    arr=new int[size];
-    front=0;
-    tail=-1;
-    count=0;
-}
-
-void Queue::enqueue(int x){
-    if(tail>=size-1){
-        cout<<"OVERFLOW"<<endl;
-        exit(EXIT_FAILURE);
-    }
-    tail=(tail+1)%size;
-    arr[tail]=x;
-    size++;
-    count++;
-}
-
-int Queue::dequeue(){
-    if(tail<0){
-        cout<<"UNDERFLOW"<<endl;
-    }
-
-    int y=arr[front];
-    front=(front+1)%size;
-    size--;
-    count--;
-    return y;
-}
-
-int Queue::peek(){
-    return arr[front];
-}
-
 int main(){
-    Queue q(6);
-    q.enqueue(5);
-    q.enqueue(6);
-    q.enqueue(7);
-    q.enqueue(8);
-    q.enqueue(9);
+    Queue q;
     q.enqueue(10);
+    q.enqueue(20);
+    q.enqueue(30);
+    q.enqueue(40);
+    q.enqueue(50);
+    q.enqueue(60);
+    q.enqueue(70);
+    q.enqueue(80);
+    q.enqueue(90);
+    q.enqueue(100);
+    q.enqueue(110);
 
     q.dequeue();
     q.dequeue();
     q.dequeue();
     q.dequeue();
     q.dequeue();
-    
-    cout<<q.peek()<<endl;
+
+    q.dequeue();
+    q.dequeue();
+
+    q.peek();
     return 0;
 }
