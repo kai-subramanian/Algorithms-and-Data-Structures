14d76283c508cf98221d7633e45d438a0aaf957b
kai-subramanian <skailash27082k@gmail.com>
2023-05-11 10:40:36 +0530

    Added adjacency matrix representation for graph

--- Java/Graphs/graph.java
+++ Java/Graphs/graph.java
@@ -1,16 +1,42 @@
 package Graphs;
 import java.util.*;
+class Vertex{
+    int v;
+    int e;
+    int mat[][];
+    public Vertex(int n){
+        this.v=n;
+        this.e=0;
+        this.mat=new int[n][n];
+    }
+    public void addEdge(int u,int v){
+        mat[u][v]=1;
+        mat[v][u]=1;
+        e+=1;
+    }
+    
+}
 public class graph {
-    public static void main(String[] args) {
-        int n=5;
-        int m=6;
-        int g[][]=new int[n+1][m+1];
-        Scanner s=new Scanner(System.in);
-        for(int i=0;i<m;i++){
-            int u=s.nextInt();
-            int v=s.nextInt();
-            g[u][v]=1;
-            g[v][u]=1;
+    static void printGraph(int num[][]){
+        for(int i=1;i<=num.length;i++){
+            System.out.print(" "+i+"");
         }
+        System.out.println();
+        for(int i=0;i<num.length;i++){
+            System.out.print(i+1+" ");
+            for(int j=0;j<num.length;j++){
+                System.out.print(num[i][j]+" ");
+            }
+            System.out.println();
+        }
+    }
+    public static void main(String[] args) {
+        Vertex g=new Vertex(5);
+        g.addEdge(1, 2);
+        g.addEdge(1, 3);
+        g.addEdge(2, 4);
+        g.addEdge(3, 4);
+        g.addEdge(2, 3);
+        printGraph(g.mat);
     }    
 }
