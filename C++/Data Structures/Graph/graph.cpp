be0c263d627734335f712ab28e6e1bc10939c89c
kai-subramanian <skailash27082k@gmail.com>
2023-05-13 23:24:41 +0530

    Modified testcase a bit

--- C++/Data Structures/Graph/graph.cpp
+++ C++/Data Structures/Graph/graph.cpp
@@ -1,19 +1,19 @@
 #include<vector>
-#include<list>
 #include<iostream>
 class Graph{
  public:
     int v;
     int e;
-    std::vector<std::list<int>> adjList;
+    std::vector<std::vector<int>> adjList;
     Graph(int x){
-        this->v=x;
-        this->e=0;
+        v=x;
+        e=0;
         adjList.resize(v);
     }
     void addEdge(int u, int v){
         adjList[u].push_back(v);
         adjList[v].push_back(u);
+        e++;
     }
     int k=0;
     void dispGraph(){
@@ -33,6 +33,7 @@ int main(){
     g.addEdge(0,1);
     g.addEdge(0,2);
     g.addEdge(1,3);
+    g.addEdge(1,2);
     g.addEdge(2,3);
     g.addEdge(3,4);
     g.addEdge(4,0);
